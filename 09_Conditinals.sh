bf1e4c8d956c7f43e9b765ad9debe0bd75c77c05
Deepak <sman59472@gmail.com>
2026-07-17 19:09:23 +0530

    for loop

--- 09_Conditinals.sh
+++ 09_Conditinals.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 read -p "enter the name: " bandi
-
+read -p "enter your love percentage: " pyar
 if [[ $bandi == "her" ]];
 then 
 	echo "Your are loyal"
+elif [[ $pyar -ge 100 ]];
+then
+	echo "You are loyal"
 else 
 	echo "Your are not loyal"
 fi
